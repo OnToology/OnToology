9b760039b9821c33d04d4fcd9e112b848f6c5270
Ahmad Alobaid <ahmad88me@gmail.com>
2015-03-16 13:21:21 +0100

    trying to get more info from the webhook

--- Auton/views.py
+++ Auton/views.py
@@ -142,7 +142,7 @@ def add_hook(request):
     h = Webhook()
     #js_req = json.loads(str(request.body))
     #h.msg = str(request.body)
-    h.msg = request.POST
+    h.msg = str(request.POST)
     h.save()
     return hooks(request)
 
