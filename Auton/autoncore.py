b4fa3e99e56ec31742716564a431e25ffe4c9367
Ahmad Alobaid <ahmad88me@gmail.com>
2015-03-18 16:07:55 +0100

    added some print statements to debug the push

--- Auton/autoncore.py
+++ Auton/autoncore.py
@@ -62,8 +62,10 @@ def fork_repo(target_repo,username,password):
 
 
 def clone_repo(cloning_repo,user):
-    call(["rm","-Rf",home+parent_folder], shell=True)
-    call(["git","clone",cloning_repo,home+parent_folder], shell=True)
+    print "rm"," -Rf "+home+parent_folder
+    call("rm"," -Rf "+home+parent_folder, shell=True)
+    print "git"+" clone"+" "+cloning_repo+" "+home+parent_folder
+    call("git"+" clone"+" "+cloning_repo+" "+home+parent_folder, shell=True)
 
 
 
