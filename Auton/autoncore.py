fdc1bbbc0897e194da43d03efa3395537914b42b
Ahmad Alobaid <ahmad88me@gmail.com>
2015-03-18 04:00:03 +0100

    added git globals

--- Auton/autoncore.py
+++ Auton/autoncore.py
@@ -72,8 +72,11 @@ def update_readme(changed_files):
 
 def commit_changes():
     print "command: "+"git --git-dir="+home+parent_folder+"/"+".git add "+home+parent_folder+"/README.md "
-    call("cd "+home+parent_folder+"/"+"; git add README.md ",shell=True)
-    call("cd "+home+parent_folder+"/"+"; git commit -m 'automated change' ",shell=True)
+    
+    call("git config --global user.email \"ahmad88csc@gmail.com\" ",shell=True)
+    call("git config --global user.name \"AutonUser\" ",shell=True)
+    call("cd "+home+parent_folder+""+"; git add README.md ",shell=True)
+    call("cd "+home+parent_folder+""+"; git commit -m 'automated change' ",shell=True)
 
 
 
