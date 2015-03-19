4f7e0ec48d9e19781da206817a1b7129f22da17b
Ahmad Alobaid <ahmad88me@gmail.com>
2015-03-19 01:22:09 +0100

    add added files and removed files to changed files

--- Auton/views.py
+++ Auton/views.py
@@ -117,6 +117,8 @@ def add_hook(request):
     target_repo = j['repository']['full_name']
     user = j['repository']['owner']['email']
     changed_files = j['head_commit']['modified']
+    changed_files+= j['head_commit']['removed']
+    changed_files+= j['head_commit']['added']
     print '##################################################'
     print 'changed_files: '+str(changed_files)
     # cloning_repo should look like 'git@github.com:AutonUser/target.git'
