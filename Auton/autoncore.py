8650cbc8d27932770bb23c955b6d45c830b4d487
Ahmad Alobaid <ahmad88me@gmail.com>
2015-04-06 00:34:05 +0200

    fix condition in get_auton_config

--- Auton/autoncore.py
+++ Auton/autoncore.py
@@ -326,7 +326,7 @@ def get_auton_configuration():
     ar2dtool_enable = True
     widoco_enable = False
     
-    if len(config.read(home+parent_folder+'/auton.cfg')) ==0:
+    if len(config.read(home+parent_folder+'/auton.cfg')) !=0:
         print 'auton configuration file does exists'
         try:
             ar2dtool_enable = config.getboolean(ar2dtool_sec_name,'enable')
