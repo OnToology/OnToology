954631105e17ea259d9a09d165500b9ed8a23b1d
Ahmad Alobaid <ahmad88me@gmail.com>
2015-05-09 15:27:45 +0200

    access profile avatar session url from template

--- Auton/settings.py
+++ Auton/settings.py
@@ -83,7 +83,7 @@ TEMPLATE_CONTEXT_PROCESSORS=(
     "django.template.context_processors.media",
     "django.template.context_processors.static",
     "django.template.context_processors.tz",
-    "django.contrib.messages.context_processors.messages"
+    "django.contrib.messages.context_processors.messages",
     "django.template.context_processors.request"
 )
 
