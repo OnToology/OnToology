88047d91954b872beb2b6fbc257c8b6b08c9cf21
Ahmad Alobaid <ahmad88me@gmail.com>
2015-05-09 21:20:36 +0200

    fix flucting profile image

--- Auton/views.py
+++ Auton/views.py
@@ -89,8 +89,8 @@ def home(request):
 #         r['monitoring'] = monit
 #         repos.append(r)
     repos = get_repos_formatted(Repo.objects.all())
-    if not request.user.is_authenticated():
-        request.session['avatar_url'] ='https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png'
+#     if not request.user.is_authenticated():
+#         request.session['avatar_url'] ='https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png'
         #request.session['avatar_url'] = 'https://assets-cdn.github.com/images/modules/logos_page/Octocat.png'
     #return render_to_response('home.html',{'repos': repos, 'user': request.user, 'avatar_url': request.session['avatar_url']},context_instance=RequestContext(request))
     #return render('home.html',{'repos': repos, 'user': request.user, 'avatar_url': request.session['avatar_url']})
