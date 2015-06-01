4fb19dde5fa2e2defc5a9bc99277edf45932b0ca
Ahmad Alobaid <ahmad88me@gmail.com>
2015-06-01 13:40:51 +0200

    fixing the error and msg in update configuration

--- OnToology/autoncore.py
+++ OnToology/autoncore.py
@@ -211,7 +211,6 @@ def verify_tools_generation_when_ready(ver_file_comp,repo=None):
    
 def update_file(target_repo,path,message,content):
     global g
-    assert False, 'testing error'
     username = os.environ['github_username']
     password = os.environ['github_password']
     if g is None:
