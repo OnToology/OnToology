9e46996b2a8ae3341ef3617c871e7c150f75d518
Ahmad Alobaid <ahmad88me@gmail.com>
2015-06-18 01:39:10 +0200

    The moment I lost the all the code I wrote for the day

--- OnToology/models.py
+++ OnToology/models.py
@@ -1,4 +1,4 @@
-from mongoengine import Document,StringField,DateTimeField,ListField,ReferenceField
+from mongoengine import Document,StringField,DateTimeField,ListField,ReferenceField, BooleanField
 #from mongoengine.django.auth import User
 from datetime import datetime
 
@@ -9,9 +9,10 @@ from datetime import datetime
 class Repo(Document):
     url = StringField(max_length=200,default='Not set yet')
     last_used = DateTimeField(default=datetime.now())
-    created_on = DateTimeField(default=datetime.now())
     state = StringField(max_length=300,default='Ready')
     owner = StringField(max_length=100,default='no')
+    previsual = BooleanField(default=False)
+    previsual_page_available = BooleanField(default=False)
 
 
 
