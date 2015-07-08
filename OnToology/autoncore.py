4371457cb38cec76d3a40aca81f56f78b70ada59
Ahmad Alobaid <ahmad88me@gmail.com>
2015-07-08 13:31:30 +0200

    added a dummy issue

--- OnToology/autoncore.py
+++ OnToology/autoncore.py
@@ -298,7 +298,9 @@ def verify_tools_generation(ver_file_comp,repo=None):
             print 'The Evaluation report of the file %s if not generated '%(ver_file_comp['file'])
     if 'not generated' in repo.state:
         repo.create_issue('OnToology error notification', repo.state)
-    
+    else:
+        repo.create_issue('OnToology testing', repo.state)
+        
     
 
 def prepare_log(user):
