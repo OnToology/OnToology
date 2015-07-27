842b03b3e47503216b11f0a17af46880b9c08a22
Ahmad Alobaid <ahmad88me@gmail.com>
2015-07-27 17:15:50 +0100

    fix issue reported by Michelle for the email not being public in add_colloborator function in autoncore

--- OnToology/autoncore.py
+++ OnToology/autoncore.py
@@ -484,7 +484,7 @@ def add_collaborator(target_repo,user,newg=None):
     global g
     if newg is None:
         newg = g
-    print 'let us try this: '+newg.get_user().email
+    #print 'let us try this: '+newg.get_user().email
     try:
         msg = newg.get_repo(target_repo).add_to_collaborators(user)
         return {'status': True, 'msg': str(msg) }
