8d58b0c24f230fcd512af2bbd08bb3e9c04055e4
Ahmad Alobaid <ahmad88me@gmail.com>
2015-07-27 21:36:29 +0100

    single issue instead of multiple if something is not generated by ontoology due to errir

--- OnToology/autoncore.py
+++ OnToology/autoncore.py
@@ -298,7 +298,11 @@ def verify_tools_generation(ver_file_comp,repo=None):
         elif not file_exists:
             print 'The Evaluation report of the file %s if not generated '%(ver_file_comp['file'])
     if 'not generated' in repo.state:
-        g.get_repo(repo.url).create_issue('OnToology error notification', repo.state)
+        repo = g.get_repo(repo.url)
+        for iss in repo.get_issues():
+            if 'OnToology error notification' in  iss.title:
+                iss.edit(state='closed')
+        repo.create_issue('OnToology error notification', repo.state)
     #else:
     #    g.get_repo(repo.url).create_issue('OnToology testing', repo.state)
         
