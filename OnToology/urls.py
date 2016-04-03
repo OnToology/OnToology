df2f2c15b52071db1cb997037e03e8e7bce87d78
Ahmad Alobaid <ahmad88me@gmail.com>
2016-04-03 02:25:07 +0200

    pip8 urls.py

--- OnToology/urls.py
+++ OnToology/urls.py
@@ -23,33 +23,28 @@ from OnToology import views
 
 from django.conf import settings
 
-
-
 urlpatterns = patterns('',
-    url(r'^delete_repo',views.delete_repo,name="deleterepo"),
-    url(r'^update_conf',views.update_conf,name='updateconf'),
-    url(r'^add_hook',views.add_hook,name='addhook'),
-    url(r'^get_access_token',views.get_access_token,name='getaccesstoken'),
-    url(r'^grantupdate',views.grant_update,name='grantupdate'),
-    url(r'^profilebeta',views.profilebeta,name='profilebeta'),
-    url(r'^profile',views.profile,name='profile'),
-    url(r'^login_get_access',views.login_get_access,name='login_get_access'),
-    url(r'^login',views.login,name='login'),
-    url(r'^logout',views.logout,name='logout'),
-    url(r'^previsual_toggle',views.previsual_toggle,name='previsualtoggle'),
-    url(r'^renew_previsual',views.renew_previsual,name='renewprevisual'),
-    url(r'^stepbystep',views.stepbystep,name='stepbystep'),
-    url(r'^generateforall',views.generateforall,name='generateforall'),
-    url(r'^about',views.about,name='about'),
-    url(r'^admin',views.superadmin,name='superadmin'),
-    url(r'',views.home, name='home'),
+    url(r'^delete_repo', views.delete_repo, name="deleterepo"),
+    url(r'^update_conf', views.update_conf, name='updateconf'),
+    url(r'^add_hook', views.add_hook, name='addhook'),
+    url(r'^get_access_token', views.get_access_token, name='getaccesstoken'),
+    url(r'^grantupdate', views.grant_update, name='grantupdate'),
+    url(r'^profilebeta', views.profilebeta, name='profilebeta'),
+    url(r'^profile', views.profile, name='profile'),
+    url(r'^login_get_access', views.login_get_access, name='login_get_access'),
+    url(r'^login', views.login, name='login'),
+    url(r'^logout', views.logout, name='logout'),
+    url(r'^previsual_toggle', views.previsual_toggle, name='previsualtoggle'),
+    url(r'^renew_previsual', views.renew_previsual, name='renewprevisual'),
+    url(r'^stepbystep', views.stepbystep, name='stepbystep'),
+    url(r'^generateforall', views.generateforall, name='generateforall'),
+    url(r'^about', views.about, name='about'),
+    url(r'^admin', views.superadmin, name='superadmin'),
+    url(r'', views.home, name='home'),
 )
-
-
-
 
 if settings.DEBUG:
     # static files (images, css, javascript, etc.)
     urlpatterns = patterns('',
         (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.MEDIA_ROOT})) + urlpatterns
+            'document_root': settings.MEDIA_ROOT})) + urlpatterns
