65c5e67126a36ec30b79dacc86af1b6d2d050df6
Ahmad Alobaid <ahmad88me@gmail.com>
2016-07-29 12:32:40 +0200

    adding status of previsualization

--- OnToology/views.py
+++ OnToology/views.py
@@ -671,6 +671,7 @@ def renew_previsual(request):
             repo = r
             break
     if found:
+        repo.state = 'Generating Previsualization'
         repo.notes = ''
         repo.previsual_page_available = True
         repo.save()
@@ -683,13 +684,18 @@ def renew_previsual(request):
         repo_dir = os.path.join(autoncore.home, folder_name)
         msg = previsual.start_previsual(repo_dir, target_repo)
         if msg == "":  # not errors
+            repo.state = 'Ready'
+            repo.save()
             return HttpResponseRedirect('/profile')
         else:
             repo.notes = msg
+            repo.state = 'Ready'
             repo.save()
             return render(request, 'msg.html', {'msg': msg})
             #return render(request, 'profile.html', {'repos': user.repos, 'pnames': PublishName.objects.filter(user=user),
             #                                'error': msg})
+    repo.state = 'Ready'
+    repo.save()
     return render(request, 'msg.html',
                   {'msg': 'You should add the repo while you are logged in before the revisual renewal'})
 
