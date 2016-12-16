8a865248fc563b6333649695856cd8b66725a995
Ahmad Alobaid <ahmad88me@gmail.com>
2016-12-16 23:35:16 +0100

    cleanup

--- OnToology/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-thisdir = os.path.dirname(__file__)
-libdir = os.path.join(thisdir, '../Integrator')
-libdir = os.path.dirname(libdir)
-if libdir not in sys.path:
-    sys.path.insert(0, libdir)
