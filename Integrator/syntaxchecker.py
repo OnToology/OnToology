d0b73c568d9c7e711e7cc95bcafd1e60ad6c6a92
Ahmad Alobaid <ahmad88me@gmail.com>
2017-08-22 15:13:54 +0200

    add syntax checker

--- Integrator/syntaxchecker.py
+++ Integrator/syntaxchecker.py
@@ -11,8 +11,8 @@ def valid_syntax(file_abs_dir):
     g = rdflib.Graph()
     try:
         g.parse(file_abs_dir, format=format_extension_mapping[file_abs_dir[-3:].lower()])
-        print "correct syntax"
-        return False
+        # print "correct syntax"
+        return True
     except Exception as e:
         print "sytax error for the file %s" % str(file_abs_dir)
         print str(e)
