d7d338b623e076ca8378f33c31640794ed94ab0e
Ahmad Alobaid <ahmad88me@gmail.com>
2018-06-19 19:45:45 +0200

    added the generate for all to the tests

--- OnToology/tests/test_api_actions.py
+++ OnToology/tests/test_api_actions.py
@@ -18,45 +18,45 @@ class TestActionAPIs(TestCase):
         self.url = 'ahmad88me/demo'
         self.user = OUser.objects.all()[0]
 
-    # as widoco create a doc for the the ones with hash e.g. myurl#abc
-    # def test_generate_all_check_generated_resources_slash(self):
-    #     import OnToology.settings as settings
-    #     resources_dir = get_repo_resource_dir(os.environ['test_user_email'])
-    #     # The below two assertion is to protect the deletion of important files
-    #     self.assertEqual(resources_dir.split('/')[-1], 'OnToology', msg='might be a wrong resources dir OnToology')
-    #     self.assertIn(os.environ['test_user_email'], resources_dir, msg='might be a wrong resources dir or wrong user')
-    #     # print "will delete %s" % resources_dir
-    #     # comm = "rm -Rf %s" % resources_dir
-    #     # print comm
-    #     settings.test_conf['local'] = True
-    #     settings.test_conf['fork'] = False
-    #     settings.test_conf['clone'] = False
-    #     settings.test_conf['push'] = False
-    #     settings.test_conf['pull'] = False
-    #     delete_all_repos_from_db()
-    #     create_repo()
-    #     c = Client()
-    #     response = c.post('/api/generate_all', {'url': Repo.objects.all()[0].url},
-    #                       HTTP_AUTHORIZATION='Token '+self.user.token)
-    #     self.assertEqual(response.status_code, 202, msg=response.content)
-    #
-    #     files_to_check = ['alo.owl/OnToology.cfg',]
-    #     docs_files = ['index-en.html', 'ontology.xml', '.htaccess', 'alo.owl.widoco.conf']
-    #     diagrams_files = ['ar2dtool-class/alo.owl.png', 'ar2dtool-taxonomy/alo.owl.png']
-    #     eval_files = ['oops.html']
-    #     for f in docs_files:
-    #         ff = os.path.join('alo.owl/documentation', f)
-    #         files_to_check.append(ff)
-    #     for f in diagrams_files:
-    #         ff = os.path.join('alo.owl/diagrams', f)
-    #         files_to_check.append(ff)
-    #     for f in eval_files:
-    #         ff = os.path.join('alo.owl/evaluation', f)
-    #         files_to_check.append(ff)
-    #     for f in files_to_check:
-    #         print os.path.join(resources_dir, f)
-    #         self.assertTrue(os.path.exists(os.path.join(resources_dir, f)), msg=(f+" does not exists"))
-    #     delete_all_repos_from_db()
+
+    def test_generate_all_check_generated_resources_slash(self):
+        import OnToology.settings as settings
+        resources_dir = get_repo_resource_dir(os.environ['test_user_email'])
+        # The below two assertion is to protect the deletion of important files
+        self.assertEqual(resources_dir.split('/')[-1], 'OnToology', msg='might be a wrong resources dir OnToology')
+        self.assertIn(os.environ['test_user_email'], resources_dir, msg='might be a wrong resources dir or wrong user')
+        # print "will delete %s" % resources_dir
+        # comm = "rm -Rf %s" % resources_dir
+        # print comm
+        settings.test_conf['local'] = True
+        settings.test_conf['fork'] = False
+        settings.test_conf['clone'] = False
+        settings.test_conf['push'] = False
+        settings.test_conf['pull'] = False
+        delete_all_repos_from_db()
+        create_repo()
+        c = Client()
+        response = c.post('/api/generate_all', {'url': Repo.objects.all()[0].url},
+                          HTTP_AUTHORIZATION='Token '+self.user.token)
+        self.assertEqual(response.status_code, 202, msg=response.content)
+
+        files_to_check = ['alo.owl/OnToology.cfg',]
+        docs_files = ['index-en.html', 'ontology.xml', '.htaccess', 'alo.owl.widoco.conf']
+        diagrams_files = ['ar2dtool-class/alo.owl.png', 'ar2dtool-taxonomy/alo.owl.png']
+        eval_files = ['oops.html']
+        for f in docs_files:
+            ff = os.path.join('alo.owl/documentation', f)
+            files_to_check.append(ff)
+        for f in diagrams_files:
+            ff = os.path.join('alo.owl/diagrams', f)
+            files_to_check.append(ff)
+        for f in eval_files:
+            ff = os.path.join('alo.owl/evaluation', f)
+            files_to_check.append(ff)
+        for f in files_to_check:
+            print os.path.join(resources_dir, f)
+            self.assertTrue(os.path.exists(os.path.join(resources_dir, f)), msg=(f+" does not exists"))
+        delete_all_repos_from_db()
 
     def test_generate_all_check_generated_resources_hash(self):
         import OnToology.settings as settings
