4f2ad3528ba8c9372d72b7478909338616002b91
Ahmad Alobaid <ahmad88me@gmail.com>
2019-02-25 11:09:50 +0100

    add stats

--- OnToology/views.py
+++ OnToology/views.py
@@ -782,6 +782,72 @@ def get_repos_list_file(request):
     return HttpResponseRedirect('/')
 
 
+def get_stats():
+    stats = {
+        'mean': 0,
+        'median': 0,
+        'num_of_ontologies': 0,
+        'num_of_repos': 0,
+        'num_of_pub': 0,
+        'num_of_reg_users': 0,
+    }
+    repos = Repo.objects.all()
+    ontologies_per_repo = []
+    num_corr_repos = 0  # number of repos that has at least one ontology
+    for r in repos:
+        ontos = get_ontologies_in_online_repo(r.url)
+        if ontos != []:
+            ontologies_per_repo.append(len(ontos))
+            num_corr_repos += 1
+    num_of_ontologies = sum(ontologies_per_repo)
+    stats['mean'] = num_of_ontologies/num_corr_repos
+    if num_corr_repos > 0:
+        ontologies_per_repo = sorted(ontologies_per_repo)
+        if num_corr_repos % 2 == 0:  # even
+            idx = (num_corr_repos-1)/2
+            stats['median'] = (ontologies_per_repo[idx] + ontologies_per_repo[idx+1]) / 2
+        else:
+            idx = num_corr_repos/2
+            stats['median'] = ontologies_per_repo[idx]
+    stats['num_of_ontologies'] = num_of_ontologies
+    stats['num_of_repos'] = len(repos)
+    stats['num_of_pub'] = len(PublishName.objects.all())
+    stats['num_of_reg_users'] = len(OUser.objects.all())
+    return stats
+
+
+@login_required
+def update_stats():
+    if request.user.email not in get_managers():
+        return render(request, 'msg.html', {'msg': 'This functionality is only available for the admins'})
+    stats = get_stats()
+    stats_html = """
+{% extends "base.html"%}
+{%block body%}
+    The average number of ontologies per repository: %d</br>
+    The median number of ontologies per repository: %d</br>
+    The total number of ontologies: %d</br>
+    The total number of repositories: %d</br>
+    The total number of published ontologies: %d</br>
+    The total number of registered users: %d</br>
+{% endblock %}
+    """ % (
+        stats['mean'],
+        stats['median'],
+        stats['num_of_ontologies'],
+        stats['num_of_repos'],
+        stats['num_of_pub'],
+        stats['num_of_reg_users']
+    )
+    stats_dir = os.path.join(settings.BASE_DIR, 'templates', 'stats.html')
+    f = open(stats_dir)
+    f.write(stats_html)
+    f.close()
+    return render(request, 'msg.html', {'msg': 'The stats file is updated'})
+
+
+def show_stats(request):
+    return render(request, 'stats.html')
 
 
 def get_managers():
