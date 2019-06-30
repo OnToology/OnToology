9a41325b741eb48665dc139c869892fb48c1d3fe
Ahmad Alobaid <ahmad88me@gmail.com>
2019-06-30 13:26:36 +0200

    revert back

--- OnToology/settings.py
+++ OnToology/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',  # From django 1.7
+    # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',  # From django 1.7
 ]
 
 ROOT_URLCONF = 'OnToology.urls'
