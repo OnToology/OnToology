dfc0d0dd76c9733bfa5ef393bf187ecd12b17f65
Ahmad Alobaid <ahmad88me@gmail.com>
2020-02-08 15:53:44 +0100

    get themis results

--- OnToology/views.py
+++ OnToology/views.py
@@ -478,7 +478,7 @@ def profile(request):
                 update_g(request.session['access_token'])
             try:
                 ontologies = parse_online_repo_for_ontologies(repo)
-                ontologies = add_themis_results(repo, ontologies)
+                ontologies = autoncore.add_themis_results(repo, ontologies)
                 print 'ontologies: ' + str(len(ontologies))
                 arepo = Repo.objects.get(url=repo)
                 pnames = PublishName.objects.filter(user=user, repo=arepo)
