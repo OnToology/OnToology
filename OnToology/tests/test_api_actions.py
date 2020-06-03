eb6274f2918cbebf3121855db98da7f531eca084
Ahmad Alobaid <ahmad88me@gmail.com>
2020-06-03 06:10:14 +0300

    run tests for generating resources with rabbitMQ

--- OnToology/tests/test_api_actions.py
+++ OnToology/tests/test_api_actions.py
@@ -3,22 +3,91 @@ import string
 import random
 import shutil
 import os
+import pika
 from subprocess import call
-from api_util import create_user, create_repo, delete_all_repos_from_db, get_repo_resource_dir, clone_if_not
+from api_util import create_user, create_repo, delete_all_repos_from_db, get_repo_resource_dir, clone_if_not, delete_all_users
+import logging
 
+from multiprocessing import Process
 from django.test import Client
 from unittest import TestCase
+from django.test.testcases import SerializeMixin
 from OnToology.models import OUser, Repo
+from OnToology.rabbit import start_pool
+from time import sleep
 
 
-class TestActionAPIs(TestCase):
+
+rabbit_host = os.environ['rabbit_host']
+queue_name = 'ontoology'
+
+
+def set_config(logger, logdir=""):
+    """
+    :param logger: logger
+    :param logdir: the directory log
+    :return:
+    """
+    if logdir != "":
+        handler = logging.FileHandler(logdir)
+    else:
+        handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+
+logger = logging.getLogger(__name__)
+set_config(logger)
+
+
+def get_pending_messages():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_host))
+    channel = connection.channel()
+    queue = channel.queue_declare(queue=queue_name, durable=True, auto_delete=True)
+    num = queue.method.message_count
+    connection.close()
+    sleep(0.1)
+    return num
+
+
+# This it to force the tests to run sequentially (while the default is to run them in parallel)
+# This is needed as the tests are executed using the same repo and user
+class Serializer(SerializeMixin):
+    lockfile = __file__
+
+    # def setUp(self):
+    #     if len(OUser.objects.all()) == 0:
+    #         create_user()
+    #     self.url = 'ahmad88me/ontoology-auto-test-no-res'
+    #     self.user = OUser.objects.all()[0]
+    #
+    #     logger.debug("rabbit host in test: "+rabbit_host)
+    #     num_of_msgs = get_pending_messages()
+    #     logger.debug("test> number of messages in the queue is: " + str(num_of_msgs))
+    #     delete_all_repos_from_db()
+
+    # def setUp(self):
+    #     pass
+
+
+class TestActionAPIs(Serializer, TestCase):
     def setUp(self):
+        delete_all_users()
         if len(OUser.objects.all()) == 0:
             create_user()
         self.url = 'ahmad88me/ontoology-auto-test-no-res'
         self.user = OUser.objects.all()[0]
 
+        logger.debug("rabbit host in test: "+rabbit_host)
+        num_of_msgs = get_pending_messages()
+        logger.debug("test> number of messages in the queue is: " + str(num_of_msgs))
+        delete_all_repos_from_db()
+
     def test_generate_all_check_generated_resources_slash(self):
+        print("###############\n\n\n\n\n##########test_generate_all_check_generated_resources_slash###############\n\n")
         import OnToology.settings as settings
         resources_dir = get_repo_resource_dir(os.environ['test_user_email'])
         # The below two assertion is to protect the deletion of important files
@@ -32,9 +101,18 @@ class TestActionAPIs(TestCase):
         settings.test_conf['clone'] = False
         settings.test_conf['push'] = False
         settings.test_conf['pull'] = False
+        print("number of repos before deleting: "+str(len(Repo.objects.all())))
+        print("number of users: "+str(len(Repo.objects.all())))
+        print("user repos: ")
+        print(OUser.objects.all()[0].repos)
         delete_all_repos_from_db()
-        create_repo()
-
+        print("number of repos after deleting: "+str(len(Repo.objects.all())))
+        print("number of users: "+str(len(Repo.objects.all())))
+        print(OUser.objects.all()[0].repos)
+        create_repo(url=self.url, user=self.user)
+        print("number of repos after creating a new one: "+str(len(Repo.objects.all())))
+        print("number of users: "+str(len(Repo.objects.all())))
+        print(OUser.objects.all()[0].repos)
         # If the setup is not to clone a fresh copy then check if it exists, if not then clone
         if settings.test_conf['clone'] is False:
             clone_if_not(resources_dir, self.url)
@@ -91,14 +169,48 @@ enable = False
         f.write(conf_file_content)
         f.close()
 
+        logger.debug("pre API> number of messages count: " + str(get_pending_messages()))
         c = Client()
+        print("printing repo: ")
+        print(Repo.objects.all()[0].json())
         response = c.post('/api/generate_all', {'url': Repo.objects.all()[0].url},
                           HTTP_AUTHORIZATION='Token '+self.user.token)
+        print("---------------\n\n\n\n\n--- IN THE MIDDLE --test_generate_all_check_generated_resources_slash###############\n\n")
         self.assertEqual(response.status_code, 202, msg=response.content)
+
+        logger.debug("post API> number of messages count: " + str(get_pending_messages()))
+        p = Process(target=start_pool)
+        p.start()
+        logger.debug("process spawn> number of messages count: " + str(get_pending_messages()))
+        while get_pending_messages()>0:
+            logger.debug("while> number of messages count: "+str(get_pending_messages()))
+        logger.debug("after while> should be executed "+str(get_pending_messages()))
+
+        self.assertEqual(1, len(Repo.objects.all()))
+        repo = Repo.objects.all()[0]
+        while repo.state != 'Ready':
+            sleep(2)
+            logger.debug('wait> status: '+repo.state)
+            logger.debug('notes: '+repo.notes)
+            repo = Repo.objects.all()[0]
 
         files_to_check = ['alo.owl/OnToology.cfg',]
         docs_files = ['index-en.html', 'ontology.xml', '.htaccess', 'alo.owl.widoco.conf']
         diagrams_files = ['ar2dtool-class/alo.owl.png', 'ar2dtool-taxonomy/alo.owl.png']
+
+        # Test block
+        cmd_p = os.path.join(resources_dir, files_to_check[0])
+        logger.debug(cmd_p)
+        cmd_pp = "/".join(cmd_p.split("/")[:-1])
+        logger.debug(cmd_pp)
+        cmd = "ls -ltra " + cmd_pp
+        logger.debug("\n\n cmd: "+cmd)
+        stream = os.popen(cmd)
+        output = stream.read()
+        logger.debug(output)
+
+
+        # os.system(cmd)
         eval_files = ['oops.html']
         # for f in docs_files:
         #     ff = os.path.join('alo.owl/documentation', f)
@@ -113,6 +225,8 @@ enable = False
             print os.path.join(resources_dir, f)
             self.assertTrue(os.path.exists(os.path.join(resources_dir, f)), msg=(f+" does not exists"))
         delete_all_repos_from_db()
+        p.terminate()
+        print("---------------\n\n\n\n\n----------test_generate_all_check_generated_resources_slash###############\n\n")
 
     # def test_generate_all_check_generated_resources_hash(self):
     #     import OnToology.settings as settings
@@ -156,6 +270,7 @@ enable = False
     #     delete_all_repos_from_db()
 
     def test_doc_multi_lang(self):
+        print("###############\n\n\n\n\n##########test_doc_multi_lang###############\n\n")
         import OnToology.settings as settings
         resources_dir = get_repo_resource_dir(os.environ['test_user_email'])
         print("resources dir: <%s>" % resources_dir)
@@ -171,7 +286,8 @@ enable = False
         settings.test_conf['push'] = False
         settings.test_conf['pull'] = False
         delete_all_repos_from_db()
-        create_repo(self.url)
+        create_repo(url=self.url, user=self.user)
+        self.assertEqual(1, len(Repo.objects.all()))
 
         # If the setup is not to clone a fresh copy then check if it exists, if not then clone
         if settings.test_conf['clone'] is False:
@@ -232,11 +348,43 @@ enable = False
         response = c.post('/api/generate_all', {'url': Repo.objects.all()[0].url},
                           HTTP_AUTHORIZATION='Token '+self.user.token)
         self.assertEqual(response.status_code, 202, msg=response.content)
-        files_to_check = ['alo.owl/OnToology.cfg', 'geolinkeddata.owl/OnToology.cfg']
+        p = Process(target=start_pool)
+        p.start()
 
+        self.assertEqual(1, len(Repo.objects.all()))
+
+        logger.debug("process spawn> number of messages count: " + str(get_pending_messages()))
+        while get_pending_messages()>0:
+            logger.debug("while> number of messages count: "+str(get_pending_messages()))
+        logger.debug("after while> should be executed "+str(get_pending_messages()))
+
+        repo = Repo.objects.all()[0]
+        while repo.state != 'Ready':
+            sleep(2)
+            logger.debug('wait> status: '+repo.state)
+            logger.debug('notes: '+repo.notes)
+            repo = Repo.objects.all()[0]
+
+
+
+
+        files_to_check = ['alo.owl/OnToology.cfg', 'geolinkeddata.owl/OnToology.cfg']
         docs_files_ = ['index-en.html', 'index-es.html', 'index-it.html', 'ontology.xml']
         docs_files_geo = docs_files_
-        docs_files_alo = docs_files_ + [ 'alo.owl.widoco.conf', '.htaccess']
+        docs_files_alo = docs_files_ + ['alo.owl.widoco.conf', '.htaccess']
+
+        # Test block
+        cmd_p = os.path.join(resources_dir, files_to_check[0])
+        logger.debug(cmd_p)
+        cmd_pp = "/".join(cmd_p.split("/")[:-1])
+        logger.debug(cmd_pp)
+        cmd = "ls -ltra " + cmd_pp
+        logger.debug("\n\n cmd: "+cmd)
+        stream = os.popen(cmd)
+        output = stream.read()
+        logger.debug(output)
+
+
 #        docs_files_alo = ['index-en.html', 'index-es.html', 'index-it.html', 'ontology.xml', '.htaccess', 'alo.owl.widoco.conf']
         # Until the issue is fixed for Widoco
         #docs_files = ['index-en.html', 'ontology.xml', '.htaccess', 'alo.owl.widoco.conf']
@@ -249,8 +397,12 @@ enable = False
         for f in ['.htaccess', 'geolinkeddata.owl.widoco.conf']:
             ff = os.path.join('geolinkeddata.owl/documentation', f)
             files_to_check.append(ff)
+        print("---------------\n\n\n\n\n----- IN THE MIDDLE -----test_doc_multi_lang###############\n\n")
         for f in files_to_check:
             fdir_to_check = os.path.join(resources_dir, f)
             print fdir_to_check
             self.assertTrue(os.path.exists(fdir_to_check), msg=(f+" does not exists"))
         delete_all_repos_from_db()
+        p.terminate()
+        print("------------------\n\n\n\n\n-------------------test_doc_multi_lang###############\n\n")
+
