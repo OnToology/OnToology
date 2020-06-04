a309865ad3b1443a2472e5e5fedfa121632348ae
Ahmad Alobaid <ahmad88me@gmail.com>
2020-06-04 15:29:13 +0300

    switch to the bash version of codecov

--- semaphoreci.sh
+++ semaphoreci.sh
@@ -1,8 +1,11 @@
-.venv/bin/pip install codecov coverage # should be added later to the base docker
-export PATH=.venv/bin/:$PATH
+#.venv/bin/pip install codecov coverage # should be added later to the base docker
+#export PATH=.venv/bin/:$PATH
 coverage run manage.py test OnToology
 coverage report
-codecov -t $CODECOV_TOKEN
+echo "CODECOV: "
+echo $CODECOV_TOKEN
+bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
+#codecov -t $CODECOV_TOKEN
 #.venv/bin/coverage run manage.py test OnToology
 #.venv/bin/coverage report
 #.venv/bin/codecov -t $CODECOV_TOKEN
