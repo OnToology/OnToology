519c9461759c5ae31fe6bf9153eb8d78a1939a19
Ahmad Alobaid <ahmad88me@gmail.com>
2020-06-04 15:54:17 +0300

    try codecov with curl

--- semaphoreci.sh
+++ semaphoreci.sh
@@ -3,7 +3,9 @@ export PATH=.venv/bin/:$PATH
 coverage run manage.py test OnToology
 coverage report
 #codecov -t $CODECOV_TOKEN
-bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
+curl -s https://codecov.io/bash > .codecov
+chmod +x .codecov
+./.codecov -t $CODECOV_TOKEN
 #.venv/bin/coverage run manage.py test OnToology
 #.venv/bin/coverage report
 #.venv/bin/codecov -t $CODECOV_TOKEN
