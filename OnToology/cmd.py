2f32e8fb4cb9bbefd601a7351b946305befa55c1
Ahmad Alobaid <ahmad88me@gmail.com>
2021-01-27 19:06:59 +0100

    fix cmd

--- OnToology/cmd.py
+++ OnToology/cmd.py
@@ -4,20 +4,22 @@ import sys
 #           TO make this app compatible with Django             #
 #################################################################
 
-proj_path = (os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
-print("proj_path: "+proj_path)
-venv_python = os.path.join(proj_path, '.venv', 'bin', 'python')
-# # This is so Django knows where to find stuff.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OnToology.settings")
-sys.path.append(proj_path)
-
-# This is so my local_settings.py gets loaded.
-os.chdir(proj_path)
-
-# This is so models get loaded.
-from django.core.wsgi import get_wsgi_application
-
-application = get_wsgi_application()
+from djangoperpmod import *
+#
+# proj_path = (os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+# print("proj_path: "+proj_path)
+# venv_python = os.path.join(proj_path, '.venv', 'bin', 'python')
+# # # This is so Django knows where to find stuff.
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OnToology.settings")
+# sys.path.append(proj_path)
+#
+# # This is so my local_settings.py gets loaded.
+# os.chdir(proj_path)
+#
+# # This is so models get loaded.
+# from django.core.wsgi import get_wsgi_application
+#
+# application = get_wsgi_application()
 
 from OnToology.models import *
 from autoncore import get_ontologies_in_online_repo
