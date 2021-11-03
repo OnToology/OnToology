65db0994a48916a28cade581b98691b6aa77d655
Ahmad Alobaid <ahmad88me@gmail.com>
2021-11-03 07:45:59 +0100

    fix semaphore to use the virtual environment for the coverage

--- semaphoreci.sh
+++ semaphoreci.sh
@@ -1,7 +1,7 @@
 sh scripts/migrate.sh
 .venv/bin/pip install --upgrade codecov # should be added later to the base docker
 export PATH=.venv/bin/:$PATH
-coverage run manage.py test OnToology --settings=OnToology.settings-tests
+.venv/bin/python -m coverage run manage.py test OnToology --settings=OnToology.settings-tests
 coverage report
 codecov -version
 codecov -t $CODECOV_TOKEN
