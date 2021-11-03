729bb50d9f874faae9c90d3c60f5926563d7da13
Ahmad Alobaid <ahmad88me@gmail.com>
2021-11-03 07:49:07 +0100

    run sqclient for the coverage tests

--- semaphoreci.sh
+++ semaphoreci.sh
@@ -1,7 +1,10 @@
 sh scripts/migrate.sh
+echo "RUN the Stiqueue Client"
+.venv/bin/python OnToology/sqclient.py &
+echo "update codecov"
 .venv/bin/pip install --upgrade codecov # should be added later to the base docker
 export PATH=.venv/bin/:$PATH
+echo "run coverage tests"
 .venv/bin/python -m coverage run manage.py test OnToology --settings=OnToology.settings-tests
 coverage report
-codecov -version
 codecov -t $CODECOV_TOKEN
